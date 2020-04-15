c1d1502062b67c693d036c1e63e4fa0aa7203073
Anil K Shah <anilsconcept@gmail.com>
2020-04-15 19:46:57 +0530

    Add new pages

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
     var btnsGroup = $(".btnsGroup");
     var btns = $(".clickMenu");
     $(btns).click(function(){
-        var pages = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18'];
+        var pages = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
         var getId = $(this).attr('id');
         console.log("Clicked button id " + getId);
         $('.sideBarOutput').load('Option1/Menu'+pages[getId-1]+'.html');
