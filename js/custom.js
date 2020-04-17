97fa748f3447468f8a90af8af59790579a9cc696
Anil K Shah <anilsconcept@gmail.com>
2020-04-17 11:28:25 +0530

    Add more pages

--- js/custom.js
+++ js/custom.js
@@ -33,13 +33,26 @@ $(document).ready(function(){
     });
 
     // Click Menu UL li
-    var btnsGroup = $(".btnsGroup");
-    var btns = $(".clickMenu");
-    $(btns).click(function(){
-        var pages = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
+   // var btnsGroup = $(".btnsGroup");
+    var btns1 = $(".clickMenu1");
+    var btns2 = $(".clickMenu2");
+    var pages1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
+    var pages2 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24'];
+    $(btns1).click(function(){
+        
         var getId = $(this).attr('id');
         console.log("Clicked button id " + getId);
-        $('.sideBarOutput').load('Option1/Menu'+pages[getId-1]+'.html');
+        $('.sideBarOutput').load('Pages/Option1/Menu'+pages1[getId-1]+'.html');
+        $( this ).parent().find( 'li.activeTab' ).removeClass( 'activeTab' );
+        $( this ).addClass( 'activeTab' );
+        
+    });
+
+    $(btns2).click(function(){
+        
+        var getId = $(this).attr('id');
+        console.log("Clicked button id " + getId);
+        $('.sideBarOutput').load('Pages/Option2/Menu'+pages2[getId-20]+'.html');
         $( this ).parent().find( 'li.activeTab' ).removeClass( 'activeTab' );
         $( this ).addClass( 'activeTab' );
         
